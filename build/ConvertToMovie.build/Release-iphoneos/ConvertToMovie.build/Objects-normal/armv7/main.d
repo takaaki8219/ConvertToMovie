364e1f68dcd1ed63a1d5ff611626f5a9104c9ae2
takaaki8219 <takaaki.kakinuma@gmail.com>
2012-12-05 19:09:04 +0900

    -hoge

--- build/ConvertToMovie.build/Release-iphoneos/ConvertToMovie.build/Objects-normal/armv7/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/takaaki/Dropbox/MacSync/test/ConvertToMovie/ConvertToMovie/main.m \
-  /Users/takaaki/Dropbox/MacSync/test/ConvertToMovie/ConvertToMovie/AppDelegate.h
--- build/ConvertToMovie.build/Release-iphoneos/ConvertToMovie.build/Objects-normal/armv7s/AppDelegate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/takaaki/Dropbox/MacSync/test/ConvertToMovie/ConvertToMovie/AppDelegate.m \
-  /Users/takaaki/Dropbox/MacSync/test/ConvertToMovie/ConvertToMovie/AppDelegate.h \
-  /Users/takaaki/Dropbox/MacSync/test/ConvertToMovie/ConvertToMovie/ViewController.h
